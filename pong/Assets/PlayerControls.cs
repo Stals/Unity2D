1c0d9c78952e741db753e49b759d2bce2ffcfd00
Stals <superstals@gmail.com>
2014-06-28 22:40:29 +0400

    pong movement

---
+++ pong/Assets/PlayerControls.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 v = rigidbody2D.velocity;
+
+		if (Input.GetKey (moveUp)) 
+		{
+			v.y = speed;
+		}
+		else if (Input.GetKey (moveDown)) 
+		{
+			v.y = -speed;
+		}
+		else 
+		{
+			v.y = 0;
+		}
+		rigidbody2D.velocity = v;
+	}
+}
