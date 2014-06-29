0354f00bb3b608f911ec9d12064c3ada2ef062b1
Stals <superstals@gmail.com>
2014-06-29 19:09:40 +0400

    play bonus when scoring

--- pong/Assets/SideWalls.cs
+++ pong/Assets/SideWalls.cs
@@ -6,6 +6,7 @@ public class SideWalls : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D hitInfo)
 	{
 		if(hitInfo.name == "Ball") {
+			audio.Play();
 			GameManager.Score(transform.name);
 
 			hitInfo.gameObject.SendMessage("ResetBall");
