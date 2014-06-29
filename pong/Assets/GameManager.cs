327f55dba7253915d6c8e1c0849ae39b29d4607b
Stals <superstals@gmail.com>
2014-06-29 17:08:53 +0400

    display score

---
+++ pong/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	public static int playerScore1 = 0;
+	public static int playerScore2 = 0;
+
+	public GUISkin theSkin;
+
+	public static void Score(string wallName) {
+		if(wallName == "leftWall") {
+			++playerScore2;
+		} else if(wallName == "rightWall") {
+			++playerScore1;
+		}
+
+		Debug.Log("Player score");
+	}
+	
+	void OnGUI()
+	{
+		GUI.skin = theSkin;
+		GUI.Label(new Rect(Screen.width/2-150, 25, 100, 100), "" + playerScore1);
+		GUI.Label(new Rect(Screen.width/2+150, 25, 100, 100), "" + playerScore2);
+	}
+}
---
+++ pong/Assets/SideWalls.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideWalls : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D hitInfo)
+	{
+		if(hitInfo.name == "Ball") {
+			GameManager.Score(transform.name);
+		}
+	}
+}
