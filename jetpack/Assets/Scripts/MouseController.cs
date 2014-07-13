787d3b5805d961a729dd2e9f490a03ca55821886
Stals <superstals@gmail.com>
2014-07-13 23:45:41 +0400

    mouse + wall collisions

---
+++ jetpack/Assets/Scripts/MouseController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+	public float jetpackForce = 75.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () 
+	{
+		bool jetpackActive = Input.GetButton("Fire1");
+		
+		if (jetpackActive)
+		{
+			rigidbody2D.AddForce(new Vector2(0, jetpackForce));
+		}
+	}
+}
