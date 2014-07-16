2b4f05f142710a3c47238f254683e0635e495306
Stals <superstals@gmail.com>
2014-07-16 23:08:31 +0400

    particle fix

---
+++ jetpack/Assets/Scripts/ParticleSprtingLayerFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSprtingLayerFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "Player";
+		particleSystem.renderer.sortingOrder = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
