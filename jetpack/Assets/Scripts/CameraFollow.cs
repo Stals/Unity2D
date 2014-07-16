250de311e4bd6520fa6a8aaafe59651fea0e7e16
Stals <superstals@gmail.com>
2014-07-16 23:30:24 +0400

    player not centered and fixxed walls in the center

--- jetpack/Assets/Scripts/CameraFollow.cs
+++ jetpack/Assets/Scripts/CameraFollow.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 	public GameObject targetObject;
+	private float distanceToTarget;
 	// Use this for initialization
 	void Start () {
-	
+		distanceToTarget = transform.position.x - targetObject.transform.position.x;
 	}
 	
 	// Update is called once per frame
@@ -13,7 +14,7 @@ public class CameraFollow : MonoBehaviour {
 		float targetObjectX = targetObject.transform.position.x;
 		
 		Vector3 newCameraPosition = transform.position;
-		newCameraPosition.x = targetObjectX;
+		newCameraPosition.x = targetObjectX + distanceToTarget;
 		transform.position = newCameraPosition;
 	}
 }
