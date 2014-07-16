5772029239fbbcd86fabf4641b9daff096ef5259
Stals <superstals@gmail.com>
2014-07-16 23:14:54 +0400

    go horizontal

--- jetpack/Assets/Scripts/MouseController.cs
+++ jetpack/Assets/Scripts/MouseController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MouseController : MonoBehaviour {
 	public float jetpackForce = 75.0f;
+	public float forwardMovementSpeed = 3.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,13 +14,26 @@ public class MouseController : MonoBehaviour {
 	
 	}
 
-	void FixedUpdate () 
+	void FixedUpdate()
+	{
+		updateVerticalSpeed();
+		updateHorizontalSpeed();
+	}
+
+	void updateVerticalSpeed()
 	{
 		bool jetpackActive = Input.GetButton("Fire1");
 		
-		if (jetpackActive)
-		{
+		if (jetpackActive){
 			rigidbody2D.AddForce(new Vector2(0, jetpackForce));
 		}
 	}
+
+	void updateHorizontalSpeed()
+	{
+		Vector2 newVelocity = rigidbody2D.velocity;
+		newVelocity.x = forwardMovementSpeed;
+		rigidbody2D.velocity = newVelocity;
+	}
+
 }
