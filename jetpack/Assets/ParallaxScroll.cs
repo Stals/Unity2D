74f9e8076f40792000d5a25da4ac7aa89b3fdb7e
Stals <superstals@gmail.com>
2014-08-05 06:39:48 +0400

    scrolling background

---
+++ jetpack/Assets/ParallaxScroll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScroll : MonoBehaviour {
+
+    public Renderer background;
+    public Renderer foreground;
+    
+    public float backgroundSpeed = 0.02f;
+    public float foregroundSpeed = 0.06f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float backgroundOffset = Time.timeSinceLevelLoad * backgroundSpeed;
+        float foregroundOffset = Time.timeSinceLevelLoad * foregroundSpeed;
+        
+        background.material.mainTextureOffset = new Vector2(backgroundOffset, 0);
+        foreground.material.mainTextureOffset = new Vector2(foregroundOffset, 0);
+	}
+}
