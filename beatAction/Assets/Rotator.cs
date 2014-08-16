33408c3d65c66e112f187d7957042fa34b8da300
Stals <superstals@gmail.com>
2014-08-16 10:44:18 +0400

    vectr tests

---
+++ beatAction/Assets/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+
+public class Rotator : MonoBehaviour {
+
+	public float speedPerTick = 10f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Elipse1>.pointRotation += speedPerTick;
+	}
+}
--- beatAction/Assets/ShapesRenderer.cs
+++ beatAction/Assets/ShapesRenderer.cs
@@ -1,39 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Vectrosity;
 
-// TODO!!! 
-// http://www.starscenesoftware.com/vectrosity.html
+
 public class ShapesRenderer : MonoBehaviour {
 
-	public Color c1 = Color.yellow;
-	public Color c2 = Color.red;
-	private LineRenderer lineRenderer;
-
-	private float radius = 4.0f;
-	//private int size;
-
-	private int vertexCount = 60;
 
 	void Start() {
 
-		lineRenderer = gameObject.AddComponent<LineRenderer>();
-		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
-		lineRenderer.SetColors(c1, c2);
-		lineRenderer.SetWidth(2f, 2f);
-		lineRenderer.SetVertexCount(vertexCount+1);
 
 	}
 	void Update() {
 
-		float x, y = 0.0f;
-
-		for(int i = 0; i < vertexCount + 1; ++i){
-
-			float angle = ((i / (float)vertexCount) * (Mathf.PI * 2));
-
-			Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
-			lineRenderer.SetPosition(i, pos);
-
-		}
 	}
 }
