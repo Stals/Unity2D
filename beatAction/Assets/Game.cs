2d2b878a46ad31217f61e661efc98f4cc7275067
Stals <superstals@gmail.com>
2014-08-18 22:35:49 +0400

    game setup scripts

---
+++ beatAction/Assets/Game.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Song{
+
+	public Song(string _path, int _tempo, int _beat, int _beatsPerBar){
+		path = _path;
+		tempo = _tempo;
+		beat = _beat;
+		beatsPerBar = _beatsPerBar;
+	}
+
+	string path; // later
+
+	public int tempo;
+	public int beat;
+	public int beatsPerBar;
+}
+
+public class Game  {
+	private static Game instance;
+
+	List<Song> songs;
+
+	private Game() {}
+	public static Game Instance
+	{
+		get 
+		{
+			if (instance == null)
+			{
+				instance = new Game();
+			}
+			return instance;
+		}
+	}
+
+	public void init(){
+		setupSongs ();
+
+	}
+
+	private void setupSongs(){
+		songs = new List<Song>();
+		songs.Add(new Song("test", 120, 4, 4));
+	}
+
+	public Song getCurrentSong()
+	{
+		return songs[0];
+	}
+}
---
+++ beatAction/Assets/GameSetup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Game.Instance.init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
