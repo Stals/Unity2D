d663f5ecfe03caec7d062228b2db49790ecd4273
Stanislav Korotaev <superstals@gmail.com>
2014-11-08 23:27:24 +0300

    object basic prefab

--- slots/Assets/Scripts/GameManager.cs
+++ slots/Assets/Scripts/GameManager.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
 	BoardGenerator generator;
 
+	[SerializeField]
+	List<GameObject> objectPrefabs;
+
 	// Use this for initialization
 	void Start () {
-		generator = new BoardGenerator (8, 8, 5);
+		generator = new BoardGenerator (objectPrefabs.Count, 8, 5);
 		Board firstBoard = generator.generateBoard ();
 		int i = firstBoard.at (3, 3).id;
 		int l = 0;
