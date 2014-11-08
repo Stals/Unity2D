1a08f2827d167dca33cbd72374c5b08232f0ed3c
Stanislav Korotaev <superstals@gmail.com>
2014-11-09 00:44:54 +0300

    display generated

---
+++ slots/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+	int width = 8;
+	int height = 5;
+
+	BoardGenerator generator;
+	
+	[SerializeField]
+	List<GameObject> objectPrefabs;
+
+	[SerializeField]
+	float paddingX;
+
+	[SerializeField]
+	float paddingY;
+
+	float objectWidth;
+	float objectHeight;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 size = objectPrefabs [0].GetComponentInChildren<SpriteRenderer> ().bounds.size;
+		objectWidth = size.x;
+		objectHeight = size.y;
+
+		generator = new BoardGenerator (objectPrefabs.Count, width, height);
+		createBoard ();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void createBoard()
+	{
+		Board board = generator.generateBoard ();
+
+		for (int x = 0; x < width; ++x) {
+			for(int y = 0; y < height; ++y){
+				int id = board.at(x, y).id;
+				GameObject obj = (GameObject)Instantiate(objectPrefabs[id], getPosition(x, y), Quaternion.identity);
+				obj.transform.parent = transform;
+			}
+		}
+	}
+
+	Vector3 getPosition(int x, int y){
+		float xPos = 0;
+		if(x != 0){
+			xPos = (objectWidth * x) + (paddingX * x);
+		}
+
+		float yPos = 0;
+		if(y != 0){
+			yPos = (objectHeight * y) + (paddingY * y);
+		}
+
+		return new Vector3(xPos, yPos, 0);
+	}
+}
--- slots/Assets/Scripts/GameManager.cs
+++ slots/Assets/Scripts/GameManager.cs
@@ -3,18 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
-
-	BoardGenerator generator;
-
-	[SerializeField]
-	List<GameObject> objectPrefabs;
-
+	
 	// Use this for initialization
 	void Start () {
-		generator = new BoardGenerator (objectPrefabs.Count, 8, 5);
-		Board firstBoard = generator.generateBoard ();
-		int i = firstBoard.at (3, 3).id;
-		int l = 0;
 	}
 	
 	// Update is called once per frame
