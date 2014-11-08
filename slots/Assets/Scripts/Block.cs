61c45617b2537afdb5bad53d12473bc051ad6db3
Stanislav Korotaev <superstals@gmail.com>
2014-11-09 01:01:51 +0300

    determine when mouse is over

---
+++ slots/Assets/Scripts/Block.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isMouseOver ()) {
+			gameObject.SetActive(false);
+		}
+	}
+
+	bool isMouseOver()
+	{
+		if (Input.GetMouseButton(0))// GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray,Mathf.Infinity);
+			
+			foreach (RaycastHit2D hit in hits)
+			{
+				if((hit.collider != null) && (hit.collider.transform == this.gameObject.transform))
+				{
+					return true;
+				}
+				
+			}
+		}
+		return false;
+	}
+}
