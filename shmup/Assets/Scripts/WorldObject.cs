ffc0e0c1f7af740c5d7895c784a345d2c824aeb3
Stals <superstals@gmail.com>
2014-11-22 15:57:06 +0400

    spawn enemies

--- shmup/Assets/Scripts/WorldObject.cs
+++ shmup/Assets/Scripts/WorldObject.cs
@@ -6,11 +6,16 @@ public class WorldObject : MonoBehaviour {
     [SerializeField]
     float speed = 0.02f;
 
+    [SerializeField]
+    GameObject enemyPrefab;
+
     Transform _transform;
 
 	// Use this for initialization
 	void Start () {
         _transform = transform;
+
+        InvokeRepeating("SpawnEnemy", 1f, 0.5f);
 	}
 	
 	// Update is called once per frame
@@ -21,5 +26,15 @@ public class WorldObject : MonoBehaviour {
     void FixedUpdate()
     {
         _transform.position = new Vector3(_transform.position.x - speed, _transform.position.y);
+    }
+
+    void SpawnEnemy()
+    {
+        //Vector3 size= Camera.main.WorldToScreenPoint(new Vector3(Screen.width, Screen.height));
+        //float halfHeight = size.y / 2;
+        float halfHeight = 4f;
+
+        GameObject enemy = (GameObject)Instantiate(enemyPrefab, new Vector3(10f, UnityEngine.Random.Range(-halfHeight, halfHeight)), transform.rotation);
+        enemy.transform.parent = transform;
     }
 }
