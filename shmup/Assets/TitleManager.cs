2ed9767b0bab52a57b9ca64a3c742b8c17ed289e
Stals <superstals@gmail.com>
2014-11-23 19:53:13 +0300

    Title Screen

---
+++ shmup/Assets/TitleManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void startNewGame()
+    {
+        Application.LoadLevel("GameSceneManager");
+    }
+}
