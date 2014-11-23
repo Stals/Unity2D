25fef9e6c5d479b75b8a37d347b8bf79ff8d6bd1
Stals <superstals@gmail.com>
2014-11-23 20:01:54 +0300

    title screen

--- shmup/Assets/TitleManager.cs
+++ shmup/Assets/TitleManager.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class TitleManager : MonoBehaviour {
 
+    [SerializeField]
+    UILabel titleLabel;
+
 	// Use this for initialization
 	void Start () {
-	
+        titleLabel.gameObject.RotateBy(new Vector3(0, 0, -0.02f), 0.5f, 0, EaseType.easeInOutSine, LoopType.pingPong);
 	}
 	
 	// Update is called once per frame
