7959310d01a0dd104db76e94cf5a8b3b808d9507
Stals <superstals@gmail.com>
2014-11-23 19:04:01 +0300

    tutorial first iteration

---
+++ shmup/Assets/TutorialManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> panels;
+
+    public static bool isCompleted = false;
+
+	// Use this for initialization
+	void Start () {
+        if (!isCompleted)
+        {
+            showPanel(0);
+        }
+	}
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void showPanel(int index){
+        if (index != 0)
+        {
+            NGUITools.SetActive(panels[index - 1], false);
+        }
+
+        if(index >= panels.Count){
+            isCompleted = true;
+            return;
+        }
+
+         NGUITools.SetActive(panels[index], true);
+
+         Observable.Timer(TimeSpan.FromMilliseconds(getTimeForIndex(index))).Subscribe(l =>
+         {
+             showPanel(index + 1);
+         }); 
+    }
+
+    int getTimeForIndex(int index) {
+        switch (index)
+        {
+            case 0: return 3000;
+            case 1: return 4000;
+            case 2: return 4000;
+            case 3: return 4000;
+            case 4: return 6000;
+            default: return 100;
+        }
+    }   
+}
