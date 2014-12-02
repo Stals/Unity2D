bbafdc85334afb16bcef46f2d6912d2b7e26dbd6
Stals <superstals@gmail.com>
2014-12-02 23:51:12 +0300

    spawn following ui

---
+++ shmup/Assets/HUD Text/Scripts/SpawnFollowingUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFollowingUI : MonoBehaviour {
+
+    [SerializeField]
+    Camera uiCamera;
+
+    [SerializeField]
+    GameObject guiPrefab;
+
+    [HideInInspector]
+    public GameObject guiObject;
+
+	// Use this for initialization
+	void Start () {
+        createPrefab();
+	}
+
+    void createPrefab()
+    {
+        guiObject = NGUITools.AddChild(uiCamera.transform.parent.gameObject, guiPrefab);
+        UIFollowTarget followTargetScript = guiObject.AddComponent<UIFollowTarget>();
+
+        followTargetScript.gameCamera = Camera.main;
+        followTargetScript.target = transform;
+        followTargetScript.uiCamera = uiCamera;
+        followTargetScript.disableIfInvisible = false;
+    }
+
+}
