6f4e9c266910225a8cfa42b7979b492170b5bc52
Stals <superstals@gmail.com>
2015-01-17 22:36:29 +0300

    lerp rotation

--- twins/Assets/PlayerController.cs
+++ twins/Assets/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     GameObject weaponSlot;
 
+    [SerializeField]
+    float anglePerSecond = 1;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -23,17 +26,44 @@ public class PlayerController : MonoBehaviour {
 
     void updateWeaponAngle()
     {
+        float newAngle = getNewWeaponAngle().z;
+        float currentAngle = weaponSlot.transform.localEulerAngles.z;
+
+        float timeDelta =  getAngleDelta(newAngle, currentAngle) / anglePerSecond;
+
+        Vector3 angle = Vector3.zero;
+        angle.z = Mathf.LerpAngle(currentAngle, newAngle, 1f / timeDelta);
+
+        weaponSlot.transform.localEulerAngles = angle;
+    }
+
+    float getAngleDelta(float dAngle1, float dAngle2)
+    {
+
+
+
+        float delta = Mathf.Max(dAngle1, dAngle2) - Mathf.Min(dAngle1, dAngle2);
+        if (180 < delta) {
+          delta = 360 - delta;
+        
+        }
+        delta = Mathf.Abs(delta);
+
+        Debug.Log("1" + dAngle1 + " 2 " + dAngle2 + " d " + delta);
+
+        return delta;
+    }
+
+    Vector3 getNewWeaponAngle()
+    {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        Debug.Log(mousePosition);
 
         float deltaY = mousePosition.y - transform.position.y;
         float deltaX = mousePosition.x - transform.position.x;
 
         Vector3 angle = Vector3.zero;
         angle.z = Mathf.Atan2(deltaY, deltaX) * (180 / Mathf.PI);
-
-        weaponSlot.transform.localEulerAngles = angle;
+        return angle;
     }
 }
