9e377f62448a49a7f482e7b9166dc8410f607412
Stals <superstals@gmail.com>
2015-01-18 17:15:41 +0300

    colliders

---
+++ twins/Assets/WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Enemy")
+        {
+            Debug.Log("HIT!");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
