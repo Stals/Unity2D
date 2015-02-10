c7667206e356b8a7625a638cd9719db03f440dc7
Stals <superstals@gmail.com>
2015-02-10 19:53:01 +0300

    moved weapon code into WeaponSlotController

--- twins/Assets/PlayerController.cs
+++ twins/Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
-    GameObject weaponSlot;
+    WeaponSlotController weaponSlot;
 
     [SerializeField]
     float anglePerSecond = 1;
@@ -37,74 +37,13 @@ public class PlayerController : MonoBehaviour {
     {
         updateMovement();
 
-        updateWeaponAngle();
-
+        weaponSlot.updateWeaponAngle(anglePerSecond, useGamepad);
 
        /* trail.Itterate(Time.deltaTime);
         trail.UpdateTrail(Time.time, Time.deltaTime);*/
     }
 
-    void updateWeaponAngle()
-    {
-        float newAngle = getNewWeaponAngle().z;
-        float currentAngle = weaponSlot.transform.localEulerAngles.z;
 
-        float timeDelta =  getAngleDelta(newAngle, currentAngle) / anglePerSecond;
-
-        Vector3 angle = Vector3.zero;
-        angle.z = Mathf.LerpAngle(currentAngle, newAngle, 1f / timeDelta);
-
-        weaponSlot.transform.localEulerAngles = angle;
-    }
-
-    float getAngleDelta(float dAngle1, float dAngle2)
-    {
-        float delta = Mathf.Max(dAngle1, dAngle2) - Mathf.Min(dAngle1, dAngle2);
-        if (180 < delta) {
-          delta = 360 - delta;
-        
-        }
-        delta = Mathf.Abs(delta);
-
-        return delta;
-    }
-
-    Vector3 getNewWeaponAngle()
-    {
-        if (useGamepad) {
-            return getNewWeaponAngleGamepad();
-        }else{
-            return getNewWeaponAngleMouse();
-        }
-    }
-
-    Vector3 getNewWeaponAngleMouse()
-    {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        float deltaY = mousePosition.y - transform.position.y;
-        float deltaX = mousePosition.x - transform.position.x;
-
-        Vector3 angle = Vector3.zero;
-        angle.z = Mathf.Atan2(deltaY, deltaX) * (180 / Mathf.PI);
-        return angle;
-    }
-
-    Vector3 getNewWeaponAngleGamepad()
-    {
-        float rotationX = Input.GetAxis("Joy X");
-        float rotationY = - Input.GetAxis("Joy Y");
-
-        // do not move sword if stick released
-        if (rotationX == 0 && rotationY == 0) {
-            return weaponSlot.transform.localEulerAngles;
-        }
-
-        Vector3 angle = Vector3.zero;
-        angle.z = Mathf.Atan2(rotationY, rotationX) * (180 / Mathf.PI);
-        return angle;
-    }
 
     void updateMovement()
     {
