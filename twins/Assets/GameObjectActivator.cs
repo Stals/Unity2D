a1a2ebc5a3edb993fe3fd43396edc57217a2c669
s.korotaev <s.korotaev@corp.nekki.ru>
2015-02-12 17:17:47 +0300

    added ball as weapon option

---
+++ twins/Assets/GameObjectActivator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GameObjectActivator : MonoBehaviour {
+
+    [SerializeField]
+    GameObject objectToActivate;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnEnable() {
+        objectToActivate.SetActive(true);
+    }
+
+    void OnDisable()
+    {
+         objectToActivate.SetActive(false);
+    }
+
+}
