f91d8cdc66024364a71d8891e80c9081ccabc795
s.korotaev <s.korotaev@corp.nekki.ru>
2015-03-14 17:13:59 +0300

    added some camera movement

---
+++ twins/Assets/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject trackedGameObejct;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // TODO interpolate?
+        transform.position = new Vector3(trackedGameObejct.transform.position.x / 7, trackedGameObejct.transform.position.y / 7, -10);
+	}
+}
