b11cf8d82734c715a33cf5d13b4dd180007ee0fb
s.korotaev <s.korotaev@corp.nekki.ru>
2015-03-15 22:16:11 +0300

    moving bullet with new image and particle

---
+++ twins/Assets/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        transform.Translate(speed, 0, 0);
+    }
+}
