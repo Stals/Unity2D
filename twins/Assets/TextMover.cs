1282f86db71f634421f0625db47107470e7a4f7f
s.korotaev <s.korotaev@corp.nekki.ru>
2015-03-21 19:04:54 +0300

    destroy after becoming invisible

--- twins/Assets/TextMover.cs
+++ twins/Assets/TextMover.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class TextMover : MonoBehaviour {
 
+    //UIWidget widget;
+
 	// Use this for initialization
 	void Start () {
+        //widget = GetComponent<UIWidget>();
+
         gameObject.MoveBy(new Vector3(Random.Range(-0.25f, 0.25f), 0, 0), 2f, 0, true);
+
+        Destroy(gameObject, GetComponent<TweenAlpha>().duration);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
 }
