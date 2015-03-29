29eb03e96e903c70682f2af56a23d2a92ee453f2
Stals <superstals@gmail.com>
2015-03-29 13:25:12 +0300

    upd

--- gamedev/Assets/ProgressController.cs
+++ gamedev/Assets/ProgressController.cs
@@ -9,28 +9,61 @@ public class ProgressController : MonoBehaviour {
     [SerializeField]
     int maxValue = 100;
 
+    [SerializeField]
+    int currentLevel = 1;
+
     UISlider progressBar;
 
     [SerializeField]
     UILabel progressLabel;
 
+    [SerializeField]
+    UILabel levelLabel;
+
+
 	// Use this for initialization
 	void Start () {
         progressBar = GetComponent<UISlider>();
         progressBar.value = ((float)currentValue) / maxValue;
+
+        if (currentLevel == 0) {
+            levelLabel.alpha = 0;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         progressBar.value = Mathf.Lerp(progressBar.value, ((float)currentValue) / maxValue, Time.deltaTime * 5f);
+        int dislayValue = (int)Mathf.Round(progressBar.value * maxValue);
+        progressLabel.text = string.Format("{0} / {1}", dislayValue, maxValue);
 
 
-        progressLabel.text = string.Format("{0} / {1}", Mathf.Round(progressBar.value * maxValue), maxValue);
-         
+        if (isLevelupBar())
+        {
+            levelLabel.text = "L." + currentLevel.ToString();
+
+            if (dislayValue >= maxValue)
+            {
+                 currentLevel += 1;
+                 // TODO play animation
+                 // TODO update effects
+
+                 progressBar.value = 0;
+                 currentValue = 0;
+                 maxValue = (int)Mathf.Round(maxValue * 1.5f);
+             }
+        }
+
+       
 	}
 
     public void addBlock()
     {
         currentValue += 1;
+    }
+
+    bool isLevelupBar()
+    {
+        return currentLevel >= 1;
     }
 }
