c886cf8f9e2cf1bb88fb717efb3f6b383306f553
Stals <superstals@gmail.com>
2015-03-29 14:52:08 +0300

    intro screen

---
+++ gamedev/Assets/IntroController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroController : MonoBehaviour {
+
+    [SerializeField]
+    TweenAlpha tween;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void close (){
+        tween.PlayForward();
+        Destroy(gameObject, tween.duration);
+    }
+}
