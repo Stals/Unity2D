2ba65e22829980c2faf0f088f0c13d72e723fb5a
Stanislav Korotaev <superstals@gmail.com>
2015-05-17 16:32:35 +0300

    stuff

---
+++ dodgeBall/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	PinController pin_1;
+
+	[SerializeField]
+	PinController pin_2;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		updateConnection();
+	}
+
+	void updateConnection()
+	{
+		// or line setactive - 
+		if(pin_1.isSelected() && pin_2.isSelected()) {
+		
+		
+		}
+	}
+
+}
